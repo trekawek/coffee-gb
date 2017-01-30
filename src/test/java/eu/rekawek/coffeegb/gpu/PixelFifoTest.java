6536f7de1e54a8660130c700c37a0e6247ffc128
Tomek Rękawek <trekawek@gmail.com>
2017-01-30 14:26:16 +0100

    Removed redundant import.

--- src/test/java/eu/rekawek/coffeegb/gpu/PixelFifoTest.java
+++ src/test/java/eu/rekawek/coffeegb/gpu/PixelFifoTest.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import static eu.rekawek.coffeegb.gpu.PixelFifo.zip;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class PixelFifoTest {
 
