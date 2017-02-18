d7173f4c7a86dec2e9fc749f2acae58d5cd0fee5
Tomek Rękawek <trekawek@gmail.com>
2017-02-18 23:54:26 +0100

    Use the internal counter for timers.

--- src/main/java/eu/rekawek/coffeegb/timer/Counter.java
+++ src/main/java/eu/rekawek/coffeegb/timer/Counter.java
@@ -1,35 +1,37 @@
 package eu.rekawek.coffeegb.timer;
 
-import eu.rekawek.coffeegb.Gameboy;
-import eu.rekawek.coffeegb.cpu.SpeedMode;
-
 public class Counter {
 
-    private final int frequency;
-
-    private final SpeedMode speedMode;
-
-    private int clocks;
+    private int mode;
 
     private int counter;
 
-    public Counter(int frequency, SpeedMode speedMode) {
-        this.frequency = frequency;
-        this.speedMode = speedMode;
+    private int delayedCounter;
+
+    private int delay = 0;
+
+    public Counter(int mode) {
+        this.mode = mode;
     }
 
-    public boolean tick() {
-        int divider = Gameboy.TICKS_PER_SEC / frequency;
-        if (speedMode != null) {
-            divider /= speedMode.getSpeedMode();
+    public boolean onClockUpdate(int oldClock, int newClock) {
+        if (delay > 0) {
+            if (--delay == 0) {
+                counter = delayedCounter;
+            }
         }
-        if (++clocks >= divider) {
-            clocks = 0;
+
+        int modeMask = getModeMask();
+        if ((oldClock & modeMask) == modeMask && (newClock & modeMask) == 0) {
             counter = (counter + 1) & 0xff;
             return true;
         } else {
             return false;
         }
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
     }
 
     public int getCounter() {
@@ -40,8 +42,31 @@ public class Counter {
         this.counter = counter;
     }
 
-    public void reset() {
+    public void setDelayedCounter(int delayedCounter) {
         this.counter = 0;
-        this.clocks = 0;
+        this.delayedCounter = delayedCounter;
+        this.delay = 4;
+    }
+
+    public void updateDelayedCounter(int delayedCounter) {
+        this.delayedCounter = delayedCounter;
+    }
+
+    public boolean isReloading() {
+        return delay > 0;
+    }
+
+    public int getModeMask() {
+        switch (mode) {
+            case 0b00:
+                return 1 << 9;
+            case 0b01:
+                return 1 << 3;
+            case 0b10:
+                return 1 << 5;
+            case 0b11:
+                return 1 << 7;
+        }
+        throw new IllegalStateException();
     }
 }
--- src/main/java/eu/rekawek/coffeegb/timer/Timer.java
+++ src/main/java/eu/rekawek/coffeegb/timer/Timer.java
@@ -7,34 +7,52 @@ import eu.rekawek.coffeegb.cpu.SpeedMode;
 
 public class Timer implements AddressSpace {
 
+    private final SpeedMode speedMode;
+
     private final Counter div;
+
+    private int divClock;
 
     private final InterruptManager interruptManager;
 
-    private final SpeedMode speedMode;
-
     private Counter tima;
+
+    private int timaClock;
 
     private int tma;
 
     private int tac;
 
+    private int timaTickDivider;
+
     public Timer(InterruptManager interruptManager, SpeedMode speedMode) {
-        this.interruptManager = interruptManager;
         this.speedMode = speedMode;
-        this.div = new Counter(16384, null); // this one is actually affected by the speedmode
-        div.setCounter(0x23);
-        setTima();
+        this.interruptManager = interruptManager;
+        this.div = new Counter(0b11);
+        this.tima = new Counter(0b00);
     }
 
     public void tick() {
-        div.tick();
-        if ((tac & (1 << 2)) != 0) {
-            boolean updated = tima.tick();
-            if (updated && tima.getCounter() == 0) {
-                tima.setCounter(tma);
-                interruptManager.requestInterrupt(InterruptType.Timer);
-            }
+        div.onClockUpdate(divClock, divClock + 1);
+        divClock = (divClock + 1) & 0x3ff;
+
+        if (speedMode.getSpeedMode() == 2 && ++timaTickDivider < 2) {
+            return;
+        }
+        timaTickDivider = 0;
+
+        updateTima(timaClock, timaClock + 1);
+        timaClock = (timaClock + 1) & 0x3ff;
+    }
+
+    private void updateTima(int oldClock, int newClock) {
+        if ((tac & (1 << 2)) == 0) {
+            return;
+        }
+        boolean updated = tima.onClockUpdate(oldClock, newClock);
+        if (updated && tima.getCounter() == 0) {
+            tima.setDelayedCounter(tma);
+            interruptManager.requestInterrupt(InterruptType.Timer);
         }
     }
 
@@ -47,20 +65,28 @@ public class Timer implements AddressSpace {
     public void setByte(int address, int value) {
         switch (address) {
             case 0xff04:
-                div.reset();
+                div.setCounter(0);
+                updateTima(timaClock, 0);
+                divClock = 0;
+                timaClock = 0;
                 break;
 
             case 0xff05:
-                tima.setCounter(value);
+                if (!tima.isReloading()) {
+                    tima.setCounter(value);
+                }
                 break;
 
             case 0xff06:
                 tma = value;
+                if (tima.isReloading()) {
+                    tima.updateDelayedCounter(tma);
+                }
                 break;
 
             case 0xff07:
                 tac = value;
-                setTima();
+                tima.setMode(tac & 0b11);
                 break;
 
         }
@@ -82,27 +108,5 @@ public class Timer implements AddressSpace {
                 return tac | 0b11111000;
         }
         throw new IllegalArgumentException("Illegal address: " + Integer.toHexString(address));
-    }
-
-    public void setTima() {
-        int freq = 4096;
-        switch (tac & 0b11) {
-            case 0b00:
-                freq = 4096;
-                break;
-
-            case 0b01:
-                freq = 262144;
-                break;
-
-            case 0b10:
-                freq = 65536;
-                break;
-
-            case 0b11:
-                freq = 16384;
-                break;
-        }
-        tima = new Counter(freq, speedMode);
     }
 }
