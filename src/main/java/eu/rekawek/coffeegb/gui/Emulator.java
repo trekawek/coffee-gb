75d2ab28dc358928c6b987d92cca74132b2f5e32
Tomek Rękawek <trekawek@gmail.com>
2018-05-24 20:54:30 +0200

    Add Title for macOS bar #5

---
+++ src/main/java/eu/rekawek/coffeegb/gui/Emulator.java
@@ -0,0 +1,141 @@
+package eu.rekawek.coffeegb.gui;
+
+import eu.rekawek.coffeegb.Gameboy;
+import eu.rekawek.coffeegb.GameboyOptions;
+import eu.rekawek.coffeegb.controller.Controller;
+import eu.rekawek.coffeegb.cpu.SpeedMode;
+import eu.rekawek.coffeegb.debug.Console;
+import eu.rekawek.coffeegb.gpu.Display;
+import eu.rekawek.coffeegb.memory.cart.Cartridge;
+import eu.rekawek.coffeegb.serial.SerialEndpoint;
+import eu.rekawek.coffeegb.sound.SoundOutput;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class Emulator {
+
+    private static final int SCALE = 2;
+
+    private final GameboyOptions options;
+
+    private final Cartridge rom;
+
+    private final AudioSystemSoundOutput sound;
+
+    private final SwingDisplay display;
+
+    private final SwingController controller;
+
+    private final SerialEndpoint serialEndpoint;
+
+    private final SpeedMode speedMode;
+
+    private final Gameboy gameboy;
+
+    private final Optional<Console> console;
+
+    private JFrame mainWindow;
+
+    public Emulator(String[] args) throws IOException {
+        options = parseArgs(args);
+        rom = new Cartridge(options);
+        speedMode = new SpeedMode();
+        serialEndpoint = SerialEndpoint.NULL_ENDPOINT;
+        console = options.isDebug() ? Optional.of(new Console()) : Optional.empty();
+        console.map(Thread::new).ifPresent(Thread::start);
+
+        if (options.isHeadless()) {
+            sound = null;
+            display = null;
+            controller = null;
+            gameboy = new Gameboy(options, rom, Display.NULL_DISPLAY, Controller.NULL_CONTROLLER, SoundOutput.NULL_OUTPUT, serialEndpoint, console);
+        } else {
+            sound = new AudioSystemSoundOutput();
+            display = new SwingDisplay(SCALE);
+            controller = new SwingController();
+            gameboy = new Gameboy(options, rom, display, controller, sound, serialEndpoint, console);
+        }
+        console.ifPresent(c -> c.init(gameboy));
+    }
+
+    private static GameboyOptions parseArgs(String[] args) {
+        if (args.length == 0) {
+            GameboyOptions.printUsage(System.out);
+            System.exit(0);
+            return null;
+        }
+        try {
+            return createGameboyOptions(args);
+        } catch(IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+            System.err.println();
+            GameboyOptions.printUsage(System.err);
+            System.exit(1);
+            return null;
+        }
+    }
+
+    private static GameboyOptions createGameboyOptions(String[] args) {
+        Set<String> params = new HashSet<>();
+        Set<String> shortParams = new HashSet<>();
+        String romPath = null;
+        for (String a : args) {
+            if (a.startsWith("--")) {
+                params.add(a.substring(2));
+            } else if (a.startsWith("-")) {
+                shortParams.add(a.substring(1));
+            } else {
+                romPath = a;
+            }
+        }
+        if (romPath == null) {
+            throw new IllegalArgumentException("ROM path hasn't been specified");
+        }
+        File romFile = new File(romPath);
+        if (!romFile.exists()) {
+            throw new IllegalArgumentException("The ROM path doesn't exist: " + romPath);
+        }
+        return new GameboyOptions(romFile, params, shortParams);
+    }
+
+    public void run() throws Exception {
+        if (options.isHeadless()) {
+            gameboy.run();
+        } else {
+            System.setProperty("sun.java2d.opengl", "true");
+
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            SwingUtilities.invokeLater(() -> startGui());
+        }
+    }
+
+    private void startGui() {
+        display.setPreferredSize(new Dimension(160 * SCALE, 144 * SCALE));
+
+        mainWindow = new JFrame("Coffee GB: " + rom.getTitle());
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setLocationRelativeTo(null);
+
+        mainWindow.setContentPane(display);
+        mainWindow.setResizable(false);
+        mainWindow.setVisible(true);
+        mainWindow.pack();
+
+        mainWindow.addKeyListener(controller);
+
+        new Thread(display).start();
+        new Thread(gameboy).start();
+    }
+
+    private void stopGui() {
+        display.stop();
+        gameboy.stop();
+        mainWindow.dispose();
+    }
+}
