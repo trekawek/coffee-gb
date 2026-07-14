5cfb53974544d922f4e4c0a85afe216d528f0a6b
Tomek Rękawek <trekawek@gmail.com>
2026-07-14 16:03:55 +0200

    Cover SGB character transfer timing

--- core/src/test/java/eu/rekawek/coffeegb/core/sgb/SuperGameboyTest.java
+++ core/src/test/java/eu/rekawek/coffeegb/core/sgb/SuperGameboyTest.java
@@ -32,6 +32,28 @@ public class SuperGameboyTest {
         assertEquals(3, transfer.get().dataTransfer[0]);
     }
 
+    @Test
+    public void characterTransferAlsoCapturesThirdFrameAfterPacket() {
+        EventBusImpl sgbBus = new EventBusImpl(null, null, false);
+        new SuperGameboy(sgbBus);
+        AtomicReference<Commands.ChrTrnCmd> transfer = new AtomicReference<>();
+        sgbBus.register(transfer::set, Commands.ChrTrnCmd.class);
+
+        int[] packet = new int[16];
+        packet[0] = (0x13 << 3) | 1; // one-packet CHR_TRN
+        packet[1] = 1; // upper tile half
+        sgbBus.post(new SuperGameboy.PacketReceivedEvent(packet));
+
+        postFrame(sgbBus, 1);
+        assertNull(transfer.get());
+        postFrame(sgbBus, 2);
+        assertNull(transfer.get());
+        postFrame(sgbBus, 3);
+
+        assertEquals(0x80, transfer.get().getTileOffset());
+        assertEquals(3, transfer.get().dataTransfer[0]);
+    }
+
     private static void postFrame(EventBusImpl sgbBus, int value) {
         int[] frame = new int[0x1000];
         Arrays.fill(frame, value);
