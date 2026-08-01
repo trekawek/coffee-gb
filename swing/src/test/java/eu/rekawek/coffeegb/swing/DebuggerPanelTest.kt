9b785955d0f9086d5ef5ec1ce368e93350dd6606
Tomek Rękawek <trekawek@gmail.com>
2026-08-01 14:37:20 +0200

    Stabilize debugger and ROM open tests

--- swing/src/test/java/eu/rekawek/coffeegb/swing/DebuggerPanelTest.kt
+++ swing/src/test/java/eu/rekawek/coffeegb/swing/DebuggerPanelTest.kt
@@ -347,7 +347,7 @@ class DebuggerPanelTest {
       val replanned = client.inspections.last().request
       assertEquals(1, replanned.anchoredRequests().size)
       assertEquals(DebugInspectionAnchor.PROGRAM_COUNTER, replanned.anchoredRequests().single().anchor())
-      assertEquals(13, replanned.anchoredRequests().single().length())
+      assertEquals(1, replanned.anchoredRequests().single().length())
     } finally {
       onEdt(panel::close)
     }
@@ -390,7 +390,7 @@ class DebuggerPanelTest {
       val replanned = client.inspections.last().request
       assertEquals(1, replanned.anchoredRequests().size)
       assertEquals(DebugInspectionAnchor.PROGRAM_COUNTER, replanned.anchoredRequests().single().anchor())
-      assertEquals(1, replanned.anchoredRequests().single().length())
+      assertEquals(13, replanned.anchoredRequests().single().length())
     } finally {
       onEdt(panel::close)
     }
