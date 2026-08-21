9ec7eaf0aeac6ccd238a871c6c40d160fd5ad8c3
Tomek Rękawek <trekawek@gmail.com>
2026-08-21 23:13:34 +0200

    Emulate GGB81 data scrambling

---
+++ core/src/main/java/eu/rekawek/coffeegb/core/memory/cart/type/Ggb81.java
@@ -0,0 +1,116 @@
+package eu.rekawek.coffeegb.core.memory.cart.type;
+
+import eu.rekawek.coffeegb.core.debug.DebugHooks;
+import eu.rekawek.coffeegb.core.events.EventBus;
+import eu.rekawek.coffeegb.core.memory.cart.MemoryController;
+import eu.rekawek.coffeegb.core.memory.cart.Rom;
+import eu.rekawek.coffeegb.core.memory.cart.battery.Battery;
+import eu.rekawek.coffeegb.core.state.ComponentState;
+import eu.rekawek.coffeegb.core.state.MachineStateCapture;
+
+/**
+ * The Vast Fame GGB81 mapper used by secondary releases of the company's games. It is
+ * MBC5-compatible, with a register that selects a data-bit permutation for the switchable ROM
+ * window. The permutation observations come from hhugboy's CC0 GGB81 mapper implementation.
+ */
+public class Ggb81 implements MemoryController {
+
+    private static final int[] IDENTITY = {0, 1, 2, 3, 4, 5, 6, 7};
+
+    private static final int[][] DATA_REORDERING = {
+            IDENTITY,
+            {0, 2, 1, 3, 4, 6, 5, 7},
+            {0, 6, 5, 3, 4, 2, 1, 7},
+            {0, 5, 1, 3, 4, 2, 6, 7},
+            {0, 5, 2, 3, 4, 1, 6, 7},
+            {0, 2, 6, 3, 4, 5, 1, 7},
+            {0, 1, 6, 3, 4, 2, 5, 7},
+            {0, 2, 5, 3, 4, 6, 1, 7}
+    };
+
+    private final Mbc5 delegate;
+
+    private int dataSwapMode;
+
+    public Ggb81(Rom rom, Battery battery) {
+        delegate = new Mbc5(rom, battery);
+    }
+
+    @Override
+    public void init(EventBus eventBus) {
+        delegate.init(eventBus);
+    }
+
+    @Override
+    public boolean accepts(int address) {
+        return delegate.accepts(address);
+    }
+
+    @Override
+    public void setByte(int address, int value) {
+        if ((address & 0xf0ff) == 0x2001) {
+            dataSwapMode = value & 0x07;
+        }
+        delegate.setByte(address, value);
+    }
+
+    @Override
+    public int getByte(int address) {
+        int value = delegate.getByte(address);
+        if (address >= 0x4000 && address < 0x8000) {
+            return reorderBits(value, DATA_REORDERING[dataSwapMode]);
+        }
+        return value;
+    }
+
+    private static int reorderBits(int value, int[] reorder) {
+        int result = 0;
+        for (int newBit = 0; newBit < 8; newBit++) {
+            result |= ((value >> reorder[newBit]) & 1) << newBit;
+        }
+        return result;
+    }
+
+    @Override
+    public void flushRam() {
+        delegate.flushRam();
+    }
+
+    @Override
+    public boolean isRumbleActive() {
+        return delegate.isRumbleActive();
+    }
+
+    @Override
+    public void setDebugHooks(DebugHooks hooks) {
+        delegate.setDebugHooks(hooks);
+    }
+
+    @Override
+    public ComponentState<MemoryController> captureState() {
+        return new Ggb81State(delegate.captureState(), dataSwapMode);
+    }
+
+    @Override
+    public ComponentState<MemoryController> captureState(MachineStateCapture capture) {
+        return new Ggb81State(delegate.captureState(capture), dataSwapMode);
+    }
+
+    @Override
+    public void declareMachineStatePayloads(MachineStateCapture capture) {
+        delegate.declareMachineStatePayloads(capture);
+    }
+
+    @Override
+    public void restoreState(ComponentState<MemoryController> state) {
+        if (!(state instanceof Ggb81State mem)) {
+            throw new IllegalArgumentException("Invalid state type");
+        }
+        delegate.restoreState(mem.delegateState);
+        dataSwapMode = mem.dataSwapMode;
+    }
+
+    private record Ggb81State(ComponentState<MemoryController> delegateState, int dataSwapMode)
+            implements ComponentState<MemoryController> {
+    }
+}
